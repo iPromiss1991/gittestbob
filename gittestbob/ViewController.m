103b43f670c9e06fa9584c129a2bcc055625942d
iPromiss1991 <904970202@qq.com>
2016-11-05 14:34:43 +0800

    测试3

--- gittestbob/ViewController.m
+++ gittestbob/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self printtest1];
+    // Do any additional se1tup after loading the view, typically from a nib.
 }
 
 
